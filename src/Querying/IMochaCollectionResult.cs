adf1549aca657b7b47a64450ea7f69b8e50f41b8
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-03-16 21:39:49 +0300

    Remove project files.

--- src/Querying/IMochaCollectionResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MochaDB.Querying {
-    /// <summary>
-    /// Interface for MochaDB collection results.
-    /// </summary>
-    /// <typeparam name="T">Value type.</typeparam>
-    public interface IMochaCollectionResult<T>:IMochaReadonlyCollection<T>, IMochaQueryableCollection<T>, IMochaResult {
-    }
-}
