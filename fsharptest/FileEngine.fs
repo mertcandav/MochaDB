49af3bf9d29ea3d2b221ca1b6095b9a613e25a8d
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-03-18 20:30:30 +0300

    Update .gitignore

---
+++ fsharptest/FileEngine.fs
@@ -0,0 +1,31 @@
+﻿module FileEngine
+
+//Libraries
+open System.IO
+open System.Text
+
+//Returns true if file is exists, returns false if not.
+//path: Path of file.
+let ExistsFile(path: string) : bool =
+    if File.Exists path = false then
+        printfn "ERROR: File is not exists!"
+        false
+    else
+        true
+
+//Returns file content with UTF8.
+//path: Path of file.
+let GetFileContent(path: string) : string =
+    if File.Exists path = false then
+        ""
+    else
+        File.ReadAllText(path,Encoding.UTF8)
+
+//Set file content with UTF8.
+//path: Path of file.
+//content: Content to set.
+let SetFileContent(path: string,content: string) =
+    if File.Exists path = false then
+        printf ""
+    else
+        File.WriteAllText(path,content,Encoding.UTF8)
