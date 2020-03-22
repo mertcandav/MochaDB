9e48b9ae618cacbdf9e7c8389b99c00cf1fd2606
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-03-22 13:10:01 +0300

    Add exception handling for mhql vb test app.

--- vbnetmhql/app.vb
+++ vbnetmhql/app.vb
@@ -4,27 +4,34 @@ Imports MochaDB.Mhql
 
 Public Class app
     Private Sub codebox_KeyDown(sender As Object, e As KeyEventArgs) Handles codebox.KeyDown
-        If e.KeyCode = Keys.F5 Then
-            Dim path = New MochaPath(Directory.GetCurrentDirectory)
-            path.ParentDirectory()
-            path.ParentDirectory()
-            path.ParentDirectory()
-            Dim db As MochaDatabase = New MochaDatabase($"Path={path.Path}/testdocs/testdb; Password=; AutoConnect=True")
+        Dim path = New MochaPath(Directory.GetCurrentDirectory)
+        path.ParentDirectory()
+        path.ParentDirectory()
+        path.ParentDirectory()
+        Dim db As MochaDatabase = New MochaDatabase($"Path={path.Path}/testdocs/testdb; Password=; AutoConnect=True")
 
-            Dim command As MochaDbCommand = New MochaDbCommand(codebox.Text, db)
-            Dim result As MochaTableResult = command.ExecuteScalar
+        Try
+            If e.KeyCode = Keys.F5 Then
+                Dim command As MochaDbCommand = New MochaDbCommand(codebox.Text, db)
+                Dim result As MochaTableResult = command.ExecuteScalar
 
-            datasource.Columns.Clear()
-            datasource.Rows.Clear()
+                datasource.Columns.Clear()
+                datasource.Rows.Clear()
 
-            For index = 0 To result.Columns.Count - 1
-                datasource.Columns.Add("", result.Columns.ElementAt(index).Name)
-            Next
-            For index = 0 To result.Rows.Count - 1
-                datasource.Rows.Add(result.Rows.ElementAt(index).Datas.ToArray)
-            Next
+                For index = 0 To result.Columns.Count - 1
+                    datasource.Columns.Add("", result.Columns.ElementAt(index).Name)
+                Next
+                For index = 0 To result.Rows.Count - 1
+                    datasource.Rows.Add(result.Rows.ElementAt(index).Datas.ToArray)
+                Next
+            End If
+        Catch excep As MochaException
+            MessageBox.Show(excep.Message, "VB.NET MHQL Test App", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch excep As Exception
+            MessageBox.Show(excep.Message, "VB.NET MHQL Test App", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
 
-            db.Dispose()
-        End If
+        db.Dispose()
+
     End Sub
 End Class
