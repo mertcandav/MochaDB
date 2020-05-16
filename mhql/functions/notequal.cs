05a6549c7af73f80e626f24261fb861b34317927
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-16 23:00:26 +0300

    Add string support to notequal function.

--- mhql/functions/notequal.cs
+++ mhql/functions/notequal.cs
@@ -11,21 +11,14 @@
         public static bool Pass(string command,MochaRow row) {
             var parts = command.Split(',');
             if(parts.Length < 2 || parts.Length > 2)
-                throw new MochaException("NOTEQUAL function is cannot processed!");
+                throw new MochaException("EQUAL function is cannot processed!");
 
             int dex;
-            decimal range;
 
             if(!int.TryParse(parts[0].TrimStart().TrimEnd(),out dex))
-                throw new MochaException("NOTEQUAL function is cannot processed!");
-            if(!decimal.TryParse(parts[1].TrimStart().TrimEnd(),out range))
-                throw new MochaException("NOTEQUAL function is cannot processed!");
+                throw new MochaException("EQUAL function is cannot processed!");
 
-            decimal value;
-            if(!decimal.TryParse(row.Datas[dex].Data.ToString(),out value))
-                throw new MochaException("NOTEQUAL function is cannot processed!");
-
-            return value != range;
+            return parts[1].Trim() != row.Datas[dex].Data.ToString();
         }
     }
 }
