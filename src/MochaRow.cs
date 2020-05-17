63953f062a47a6ec068e14a41a689fa7f26497ad
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-17 16:19:36 +0300

    Add object params support constructor to MochaRow.

--- src/MochaRow.cs
+++ src/MochaRow.cs
@@ -32,6 +32,24 @@ namespace MochaDB {
             Datas.collection.AddRange(datas);
         }
 
+        /// <summary>
+        /// Create new MochaRow.
+        /// </summary>
+        /// <param name="datas">Datas of row.</param>
+        public MochaRow(params object[] datas) :
+            this() {
+            for(int dex = 0; dex < datas.Length; dex++) {
+                var data = datas[dex];
+                Datas.collection.Add(
+                    new MochaData() {
+                        dataType =
+                            MochaData.GetDataTypeFromType(data.GetType()),
+                        data =
+                            data
+                    });
+            }
+        }
+
         #endregion
 
         #region Properties
