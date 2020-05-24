b78e023311956587f5f172e5e01e7eb7c5c2933f
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-24 19:46:02 +0300

    Add grammar.

---
+++ mhql/grammar.cs
@@ -0,0 +1,20 @@
+namespace MochaDB.mhql {
+    /// <summary>
+    /// Grammar of MHQL.
+    /// </summary>
+    internal static class Mhql_GRAMMAR {
+        /// <summary>
+        /// Functions of must.
+        /// </summary>
+        public static string[] MustFunctions =>
+            new[] {
+                "BETWEEN",
+                "BIGGER",
+                "LOWER",
+                "EQUAL",
+                "NOTEQUAL",
+                "STARTW",
+                "ENDW"
+            };
+    }
+}
