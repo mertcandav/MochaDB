82800e6bd05e854c3a2dca513bcca3f1f17da3b9
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-06-18 10:58:04 +0300

    Change MhqlKeyword to abstract class.

--- mhql/MhqlKeyword.cs
+++ mhql/MhqlKeyword.cs
@@ -2,7 +2,7 @@ namespace MochaDB.mhql {
     /// <summary>
     /// Base class for Mhql keywords.
     /// </summary>
-    internal class MhqlKeyword:IMhqlKeyword {
+    internal abstract class MhqlKeyword:IMhqlKeyword {
         #region Properties
 
         public MochaDatabase Tdb { get; set; }
