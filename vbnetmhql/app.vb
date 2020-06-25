ffaa1252031be32af4c7aa76c9f8450c238737f1
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-06-25 18:37:05 +0300

    Update VB test app.

--- vbnetmhql/app.vb
+++ vbnetmhql/app.vb
@@ -27,14 +27,14 @@ Imports MochaDB.Mhql
 
 Public Class app
     Private Sub codebox_KeyDown(sender As Object, e As KeyEventArgs) Handles codebox.KeyDown
-        Dim path = New MochaPath(Directory.GetCurrentDirectory)
-        path.ParentDirectory()
-        path.ParentDirectory()
-        path.ParentDirectory()
-        Dim db As MochaDatabase = New MochaDatabase($"Path={path.Path}/testdocs/testdb; Password=; AutoConnect=True")
-
         Try
+            Dim db As MochaDatabase
             If e.KeyCode = Keys.F5 Then
+                Dim path = New MochaPath(Directory.GetCurrentDirectory)
+                path.ParentDirectory()
+                path.ParentDirectory()
+                path.ParentDirectory()
+                db = New MochaDatabase($"Path={path.Path}/tests/testdb; Password=; AutoConnect=True")
                 Dim command As MochaDbCommand = New MochaDbCommand(codebox.Text, db)
                 Dim mhqlcommand As MhqlCommand = codebox.Text
                 If mhqlcommand.IsReaderCompatible Then
@@ -52,14 +52,12 @@ Public Class app
                 Else
                     command.ExecuteCommand()
                 End If
+                db.Dispose()
             End If
         Catch excep As MochaException
             MessageBox.Show(excep.Message, "VB.NET MHQL Test App", MessageBoxButtons.OK, MessageBoxIcon.Error)
         Catch excep As Exception
             MessageBox.Show(excep.Message, "VB.NET MHQL Test App", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
-
-        db.Dispose()
-
     End Sub
 End Class
