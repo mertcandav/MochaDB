c5d8d2180a2217c0cd3e9684d3e09a3106681227
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-18 13:57:51 +0300

    Add multi param support to notendw function of mhql.

--- mhql/must/functions/notendw.cs
+++ mhql/must/functions/notendw.cs
@@ -13,14 +13,12 @@ namespace MochaDB.mhql.must.functions {
     /// <param name="row">Row.</param>
     /// <param name="from">Use state FROM keyword.</param>
     public static bool Pass(string command,MochaTableResult table,MochaRow row,bool from) {
-      var parts = command.Split(',');
-      if(parts.Length != 2)
-        throw new MochaException("The NOTENDW function can only take 2 parameters!");
-
+      string[] parts = command.Split(',');
       int dex = Mhql_GRAMMAR.GetIndexOfColumn(parts[0],table.Columns,from);
-
-      return
-          !row.Datas[dex].Data.ToString().EndsWith(parts[1]);
+      for(int index = 0; index < parts.Length; index++)
+        if(row.Datas[dex].Data.ToString().EndsWith(parts[index]))
+          return false;
+      return true;
     }
   }
 }
