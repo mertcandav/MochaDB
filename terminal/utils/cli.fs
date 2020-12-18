0aa20323b779fec2f6d30b90b26a85bee2b7852a
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-12-18 15:48:16 +0300

    Add MochaTableResult support to cli print

--- terminal/utils/cli.fs
+++ terminal/utils/cli.fs
@@ -5,6 +5,7 @@ open System.Linq
 open System.Collections.Generic
 
 open MochaDB
+open MochaDB.Mhql
 
 open terminal
 
@@ -18,7 +19,7 @@ type cli() =
       printf "%s" (new String(' ', maxlen - key.Length))
       printfn "%s" dict.[key]
 
-  // Print table.
+  // Print MochaTable.
   static member printTable(table:MochaTable) : unit =
     if table.IsEmpty() then
       printfn "Table is empty!"
@@ -50,3 +51,36 @@ type cli() =
       printRow(table.Columns)
       for row in table.Rows do
         printRow(row.Datas)
+
+  // Print table.
+  static member printTable(table:MochaTableResult) : unit =
+    if table.IsEmpty() then
+      printfn "Table is empty!"
+    else
+      let mutable tx = table.Columns.Sum(fun(x:MochaColumn) -> x.Name.Length)
+      let ctx = table.Columns.Max(
+        fun(x:MochaColumn) -> x.Datas.Sum(fun(y:MochaData) -> y.Data.ToString().Length))
+      tx <- if tx < ctx then ctx else tx
+
+      // Centre content.
+      let alignCentre(text:string, x:int) : string =
+        if String.IsNullOrEmpty(text) then
+          new String(' ', x)
+        else
+          text.PadRight(x - (x - text.Length) / 2).PadLeft(x)
+
+      // Print row.
+      let printRow(values:'a[]) : unit =
+        let x = (tx - values.Length) / values.Length
+        printf "|"
+        let mutable finalLine = "|"
+        for value in values do
+          let mutable value = (alignCentre(value.ToString(), x))
+          value <- value.Replace("\n", " ")
+          printf "%s|" value
+          finalLine <- finalLine + (new String('-', value.Length)) + "|"
+        printfn "\n%s" finalLine
+
+      printRow(table.Columns)
+      for row in table.Rows do
+        printRow(row.Datas.ToArray())
