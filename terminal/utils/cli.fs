54b3dcc8159b4d12bdc529001afe29edb212a096
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-12-18 15:07:36 +0300

    Add table print support

--- terminal/utils/cli.fs
+++ terminal/utils/cli.fs
@@ -4,21 +4,49 @@ open System
 open System.Linq
 open System.Collections.Generic
 
+open MochaDB
+
 open terminal
 
 // CLI module.
 type cli() =
   // Print dictionary as table.
   static member printDictAsTable(dict:IDictionary<'a,'b>) : unit =
-    // Return whitespaced string by count.
-    let getWS(count:int) : string =
-      let sb = new System.Text.StringBuilder(String.Empty)
-      for _ in 1..count do
-        sb.Append(" ") |> ignore
-      sb.ToString()
-    let maxlen = dict.Keys.Max(fun(x) -> x.ToString().Length) + 5
+    let maxlen = dict.Keys.Max(fun(x:'a) -> x.ToString().Length) + 5
     for key in dict.Keys do
       terminal.printc(key, ConsoleColor.Yellow)
-      printf "%s" (getWS(maxlen - key.Length))
+      printf "%s" (new String(' ', maxlen - key.Length))
       printfn "%s" dict.[key]
 
+  // Print table.
+  static member printTable(table:MochaTable) : unit =
+    if table.IsEmpty() then
+      printfn "Table is empty!"
+    else
+      let mutable tx = table.Columns.Sum(fun(x:MochaColumn) -> x.Name.Length)
+      let ctx = table.Columns.Max(
+        fun(x:MochaColumn) -> x.Datas.Sum(fun(y:MochaData) -> y.Data.ToString().Length))
+      tx <- if tx < ctx then ctx else tx
+
+      // Centre content.
+      let alignCentre(text:string, x:int) : string =
+        if String.IsNullOrEmpty(text) then
+          new String(' ', x)
+        else
+          text.PadRight(x - (x - text.Length) / 2).PadLeft(x)
+
+      // Print row.
+      let printRow(values:MochaCollection<'a>) : unit =
+        let x = (tx - values.Count) / values.Count
+        printf "|"
+        let mutable finalLine = "|"
+        for value in values do
+          let mutable value = (alignCentre(value.ToString(), x))
+          value <- value.Replace("\n", " ")
+          printf "%s|" value
+          finalLine <- finalLine + (new String('-', value.Length)) + "|"
+        printfn "\n%s" finalLine
+
+      printRow(table.Columns)
+      for row in table.Rows do
+        printRow(row.Datas)
