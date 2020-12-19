173c71c82aeb08ce81fc0369a0ff82df047262bf
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-12-19 18:10:47 +0300

    Update commit.bat

--- commit.bat
+++ commit.bat
@@ -21,6 +21,7 @@
 :: OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 :: SOFTWARE.
 
+@set /P message="Message: "
 @git add .
-@git commit -m %1
+@git commit -m %message%
 @git push
