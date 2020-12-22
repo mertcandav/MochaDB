c995c172dc824a35293146efa1a384943c8b89ee
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-12-23 00:28:13 +0300

    Remove IMhqlKeyword

--- mhql/keywords/MhqlKeyword.cs
+++ mhql/keywords/MhqlKeyword.cs
@@ -2,7 +2,7 @@ namespace MochaDB.mhql.keywords {
   /// <summary>
   /// Base class for Mhql keywords.
   /// </summary>
-  internal abstract class MhqlKeyword:IMhqlKeyword {
+  internal abstract class MhqlKeyword {
     #region Properties
 
     public MochaDatabase Tdb { get; set; }
