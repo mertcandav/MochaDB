ef6781f68323bf7d7c7d2037df8ab9c13509671e
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-02 13:36:38 +0300

    Add grammar

---
+++ terminal/mhsh/parser/_GRAMMAR_.fs
@@ -0,0 +1,8 @@
+﻿namespace mhsh.parser
+
+/// <summary>
+/// Grammar.
+/// </summary>
+type _GRAMMAR_() =
+  static member isValidVariableName(name:string) : bool =
+    _LEXER_.findVariableLimit(name) = -1
