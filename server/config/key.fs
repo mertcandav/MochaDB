ee8bb338e7d45c69f7f2d79de6991d40617cc104
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-07 20:34:16 +0300

    Add key.fs to server/config

---
+++ server/config/key.fs
@@ -0,0 +1,41 @@
+﻿namespace config
+
+open System
+
+/// <summary>
+/// A key instance.
+/// </summary>
+[<Struct>]
+type key =
+  struct
+  
+  /// <summary>
+  /// Name of key.
+  /// </summary>
+  val name:string
+
+  /// <summary>
+  /// Value of key.
+  /// <summary>
+  val value:string
+
+  /// <summary>
+  /// Create a new instance of <see cref="key"/>.
+  /// </summary>
+  /// <param name="name">Name of key.</param>
+  new(name:string) = {
+    name = name
+    value = String.Empty
+  }
+
+  /// <summary>
+  /// Create a new instance of <see cref="key"/>.
+  /// </summary>
+  /// <param name="name">Name of key.</param>
+  /// <param name="value">Value of key.</param>
+  new(name:string, value:string) = {
+    name = name
+    value = value
+  }
+
+  end
