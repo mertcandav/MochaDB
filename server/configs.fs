ecb7d812aefb40ae66d79a6bffced03115f4b5f7
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-07 20:22:21 +0300

    Add configs.fs to server

---
+++ server/configs.fs
@@ -0,0 +1,25 @@
+﻿namespace Server
+
+/// <summary>
+/// Config settings.
+/// </summary>
+type configs() =
+  /// <summary>
+  /// Name of server.
+  /// </summary>
+  static member val name:string = "MochaDB Server" with get, set
+
+  /// <summary>
+  /// IP address.
+  /// </summary>
+  static member val address:string = null with get, set
+
+  /// <summary>
+  /// Port.
+  /// </summary>
+  static member val port:int = 8085 with get, set
+
+  /// <summary>
+  /// The maximum length of the pending connections queue.
+  /// </summary>
+  static member val listen:int = 10 with get, set
