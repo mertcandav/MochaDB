447285c7a1694afee8360b9ac1bc6ac119b1a234
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-08 11:09:59 +0300

    Add lexer to server/config

---
+++ server/config/lexer.fs
@@ -0,0 +1,27 @@
+﻿namespace config
+
+/// <summary>
+/// Lexer of config system.
+/// </summary>
+type lexer() =
+  /// <summary>
+  /// Key type is string?
+  /// </summary>
+  /// <param name="name">Name of key.</param>
+  /// <returns>true is type of key is string, false if not.</returns>
+  static member isStringKey(name:string) : bool =
+    match name with
+    | "name"
+    | "address" -> true
+    | _ -> false
+
+  /// <summary>
+  /// Key type is integer?
+  /// </summary>
+  /// <param name="name">Name of key.</param>
+  /// <returns>true is type of key is integer, false if not.</returns>
+  static member isIntegerKey(name:string) : bool =
+    match name with
+    | "port"
+    | "listen" -> true
+    | _ -> false
